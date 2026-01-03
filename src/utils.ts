4f979ac9f456c0b1e947d8c890164544b0a05567
Mayurrajsinh Rathod <mdridrdrr@gmail.com>
2026-01-03 21:17:42 +0530

    Add custom async handler to have better control over errors

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,34 @@
-import type { Request } from "express";
-export const AuthenticatedRequest =  Request;
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
+import type { Request, Response } from "express";
+import { ZodError, z } from "zod";
+
+interface CustomHeaders {
+  uid?: number;
+}
+export interface reqObj extends Request {
+  headers: CustomHeaders & Request["headers"];
+}
+
+export const asyncHandler = <
+    T extends (req: reqObj, res: Response) => Promise<void>,
+>(
+    fn: T
+) => {
+    return async (req: reqObj, res: Response) => {
+        try {
+            return await fn(req, res);
+        } catch (err) {
+            if (err instanceof ZodError) {
+                res.status(400).json({ message: z.prettifyError(err) });
+            } else {
+                let message = "Internal Server Error";
+                if (err instanceof PrismaClientKnownRequestError) {
+                    message = err.message
+                }
+                res.status(500).json({ message });
+            }
+            console.error(err);
+            return;
+        }
+    };
+};
