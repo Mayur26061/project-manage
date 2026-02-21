62bcef94aab8cfda7df097befd00383daac55db1
Mayurrajsinh Rathod <mdridrdrr@gmail.com>
2026-02-21 15:35:21 +0530

    use useReducer for project form handling

--- frontend/src/routes/_pathlessLayout/projects/$projectId/index.tsx
+++ frontend/src/routes/_pathlessLayout/projects/$projectId/index.tsx
@@ -12,7 +12,7 @@ import axios from "axios";
 import { ChevronDownIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import RecordSelector from "@/components/RecordSelector";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { format } from "date-fns/format";
 export const Route = createFileRoute("/_pathlessLayout/projects/$projectId/")({
   component: ProjectComponent,
@@ -28,18 +28,84 @@ interface Project {
   owner_id: number;
   date_end: Date | null;
   owner: { id: number; name: string };
+  customer_id: number | null;
   customer: { id: number; name: string } | null;
 }
+
+type actions =
+  | { type: "SET_NAME"; payload: Project["name"] }
+  | { type: "SET_DESCRIPTION"; payload: Project["description"] }
+  | { type: "SET_OWNER_ID"; payload: Project["owner"] }
+  | { type: "SET_DATE_END"; payload: Project["date_end"] }
+  | { type: "SET_CUSTOMER_ID"; payload: Project["customer"] }
+  | { type: "SET_INITIAL"; payload: Project };
 
 function ProjectComponent() {
   const [project, setProject] = useState<Project | null>(null);
   const [calendarOpen, setCalendarOpen] = useState(false);
   const params = useParams({ from: "/_pathlessLayout/projects/$projectId/" });
 
+  const formReducerFunction = (state: Project, action: actions) => {
+    switch (action.type) {
+      case "SET_NAME":
+        return { ...state, name: action.payload };
+      case "SET_DESCRIPTION": {
+        return { ...state, description: action.payload };
+      }
+      case "SET_OWNER_ID": {
+        return {
+          ...state,
+          owner: action.payload,
+          owner_id: action.payload.id,
+        };
+      }
+      case "SET_CUSTOMER_ID": {
+        return {
+          ...state,
+          customer: action.payload || null,
+          customer_id: action.payload?.id || null,
+        };
+      }
+      case "SET_DATE_END": {
+        return { ...state, date_end: action.payload };
+      }
+      case "SET_INITIAL": {
+        return { ...state, ...action.payload };
+      }
+      default:
+        return state;
+    }
+  };
+
+  const [formData, dispatch] = useReducer<Project, [action: actions]>(
+    formReducerFunction,
+    {
+      id: 0,
+      created_at: new Date(),
+      updated_at: new Date(),
+      active: false,
+      name: "",
+      description: null,
+      owner_id: 0,
+      date_end: null,
+      owner: { id: 0, name: "" },
+      customer_id: null,
+      customer: { id: 0, name: "" },
+    },
+  );
+ const onSaveChanges = () => {
+   
+ };
+ 
+ const onDiscardChanges = () => {
+  dispatch({ type: "SET_INITIAL", payload: project! });
+ };
+
   useEffect(() => {
-    axios
-      .get(`/api/project/${params.projectId}`)
-      .then((response) => setProject(response.data.project));
+    axios.get(`/api/project/${params.projectId}`).then((response) => {
+      dispatch({ type: "SET_INITIAL", payload: response.data.project });
+      setProject(response.data.project);
+    });
   }, [params.projectId]);
   if (!project) return <div>Loading...</div>;
 
@@ -48,7 +114,14 @@ function ProjectComponent() {
       <div className="flex items-center justify-between gap-5">
         <div className="w-1/2">
           <Label>Project Name</Label>
-          <Input className="my-2" type="text" value={project.name} />
+          <Input
+            className="my-2"
+            type="text"
+            value={formData.name}
+            onChange={(ev) => {
+              dispatch({ type: "SET_NAME", payload: ev.target.value });
+            }}
+          />
         </div>
         <div className="w-1/2">
           <Label>DeadLine</Label>
@@ -56,11 +129,11 @@ function ProjectComponent() {
             <PopoverTrigger asChild className="my-2 ">
               <Button
                 variant={"outline"}
-                // data-empty={!formData.deadline}
+                data-empty={!formData.date_end}
                 className="data-[empty=true]:text-muted-foreground w-full justify-between text-left font-normal"
               >
-                {project.date_end ? (
-                  format(new Date(project.date_end), "PPP")
+                {formData.date_end ? (
+                  format(new Date(formData.date_end), "PPP")
                 ) : (
                   <span>Pick a date</span>
                 )}
@@ -71,14 +144,14 @@ function ProjectComponent() {
               <Calendar
                 mode="single"
                 selected={
-                  project.date_end ? new Date(project.date_end) : undefined
+                  formData.date_end ? new Date(formData.date_end) : undefined
                 }
-                onSelect={() => {
-                  // dispatch({ type: "SET_DEADLINE", payload: ev || null });
+                onSelect={(date) => {
+                  dispatch({ type: "SET_DATE_END", payload: date || null });
                   setCalendarOpen(false);
                 }}
                 defaultMonth={
-                  project.date_end ? new Date(project.date_end) : new Date()
+                  formData.date_end ? new Date(formData.date_end) : new Date()
                 }
               />
             </PopoverContent>
@@ -89,17 +162,21 @@ function ProjectComponent() {
         <div className="w-1/2">
           <Label>Customer</Label>
           <RecordSelector
-            data={project.customer || null}
+            data={formData.customer || null}
             model="user"
-            setData={() => { }}
+            setData={(customer) => {
+              dispatch({ type: "SET_CUSTOMER_ID", payload: customer });
+            }}
           />
         </div>
         <div className="w-1/2">
           <Label>Project Manager</Label>
           <RecordSelector
-            data={project.owner}
+            data={formData.owner}
             model="user"
-            setData={() => { }}
+            setData={(owner) => {
+              dispatch({ type: "SET_OWNER_ID", payload: owner });
+            }}
           />
         </div>
       </div>
@@ -107,11 +184,19 @@ function ProjectComponent() {
         <Label>Description</Label>
         <Textarea
           className=" min-h-96 my-3"
-          value={project.description || ""}
+          value={formData.description || ""}
+          onChange={(ev) =>
+            dispatch({ type: "SET_DESCRIPTION", payload: ev.target.value })
+          }
           placeholder="Enter Description here"
         />
       </div>
-      {project && JSON.stringify(project)}
+        <div className="flex gap-2">
+        <Button onClick={onSaveChanges}>Save</Button>
+        <Button variant={"outline"} onClick={onDiscardChanges}>
+          Cancel
+        </Button>
+      </div>
       <Outlet />
     </div>
   );
