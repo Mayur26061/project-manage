8e7e3617b56ce18ac1a8fe5cace1f54f510b7ffe
Mayurrajsinh Rathod <mdridrdrr@gmail.com>
2026-05-04 17:02:12 +0530

    [FIX] fix text wrap of record badge

--- frontend/src/components/RecordBadge.tsx
+++ frontend/src/components/RecordBadge.tsx
@@ -8,12 +8,20 @@ const RecordBadge = ({
   onRemove?: (ev: React.MouseEvent<SVGSVGElement>) => void;
 }) => {
   return (
-    <div className="flex items-center gap-2 flex-nowrap border border-gray-300 rounded px-2 py-1 text-sm bg-gray-100">
+    <div
+      title={name}
+      className="flex items-center gap-2 flex-nowrap border border-gray-300 px-2 py-1 text-sm bg-gray-100 rounded-l-full rounded-r-full cursor-pointer hover:bg-gray-200"
+    >
       <div className="rounded-full p-3 border-gray-300 bg-gray-200 text-gray-600 w-6 h-6 flex items-center justify-center">
         {name[0].toUpperCase()}
       </div>
-      <span>{name}</span>
-      <X className="w-4 h-4 text-gray-500 cursor-pointer" onClick={(ev) => onRemove?.(ev)} />
+      <div className="max-w-32 overflow-hidden whitespace-nowrap text-ellipsis ">
+        {name}
+      </div>
+      <X
+        className="w-4 h-4 text-gray-500 cursor-pointer"
+        onClick={(ev) => onRemove?.(ev)}
+      />
     </div>
   );
 };
